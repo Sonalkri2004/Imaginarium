070f05b81491e61d57ce0728e0b3a3e451f7c8b4
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-10-15 14:36:56 +0530

    logo updated.

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -18,12 +18,7 @@ const Topbar = () => {
     <section className="topbar">
       <div className="flex-between py-4 px-5">
         <Link to="/" className="flex gap-3 items-center">
-          <img
-            src="/assets/images/logo.svg"
-            alt="logo"
-            width={130}
-            height={325}
-          />
+        <img src="/assets/images/Imaginarium1.png" className="w-44 h-auto" alt="logo" />
         </Link>
 
         <div className="flex gap-4">
